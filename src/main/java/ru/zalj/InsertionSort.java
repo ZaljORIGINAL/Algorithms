6c2c3ab2904cf4e2af7970b4298f19a4bf08027f
ZaljORIGINAL <zalj.kod@gmail.com>
2025-08-07 22:04:02 +0300

    Описана сортировка вставками

---
+++ src/main/java/ru/zalj/InsertionSort.java
@@ -0,0 +1,15 @@
+package ru.zalj;
+
+public class InsertionSort implements Sort {
+    @Override
+    public int[] sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0 && array[j-1] > array[j]; j--) {
+                int tmp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = tmp;
+            }
+        }
+        return array;
+    }
+}
