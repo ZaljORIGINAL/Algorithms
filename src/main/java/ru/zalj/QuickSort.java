82cf4edea66f80796f78d6e81c72d09c38a0da31
ZaljORIGINAL <zalj.kod@gmail.com>
2025-08-18 23:59:11 +0300

    Описана быстрая сортировка

---
+++ src/main/java/ru/zalj/QuickSort.java
@@ -0,0 +1,36 @@
+package ru.zalj;
+
+public class QuickSort implements Sort {
+    @Override
+    public int[] sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+        return array;
+    }
+
+    private void quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(array, left, right);
+
+            quickSort(array, left, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, right);
+        }
+    }
+
+    private int partition(int[] array, int left, int right) {
+        int pivot = array[right];
+        int i = left;
+        for (int j = left; j < right; j++) {
+            if (array[j] <= pivot) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[right];
+        array[right] = temp;
+
+        return i;
+    }
+}
